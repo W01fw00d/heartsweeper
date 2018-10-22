4e635eae45f519c6dd1468892b1420708fd86592
W01fw00d <gabrm_91@hotmail.com>
2018-10-22 19:13:26 +0200

    first commit: part 1 of tutorial

---
+++ Cell.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour {
+
+    private bool hasMine = false;
+    private readonly float percentage_mines =  0.15f;
+
+    public Sprite[] emptyTextures;
+    public Sprite mineTexture;
+
+    private readonly string covered_sprite_name = "panel";
+
+	// Use this for initialization
+	void Start () {
+        hasMine = (Random.value < percentage_mines);
+
+        int x = (int) this.transform.position.x;
+        int y = (int) this.transform.position.y;
+
+        GridHelper.cells[x, y] = this;
+
+        LoadTexture(0); //Debugging
+	}
+
+
+    public bool IsCovered()
+    {
+        return GetComponent<SpriteRenderer>().sprite.texture.name == covered_sprite_name;
+    }
+
+    public void OnMouseUpAsButton()
+    {
+        if (IsCovered())
+        {
+            if (hasMine)
+            {
+                // TODO: Mostrar gameover y cerrar app
+                Debug.Log("BOOOM! Te enamoraste!");
+            }
+            else
+            {
+                // TODO: cambiar la imagen
+                // TODO: descubrir toda el área sin minas alrededor
+                // TODO: comprobar si el juego ha terminado
+            }
+        }
+    }
+
+    public void LoadTexture(int loadTextureIndex)
+    {
+        if (hasMine)
+        {
+            GetComponent<SpriteRenderer>().sprite = mineTexture;
+        } else
+        {
+            GetComponent<SpriteRenderer>().sprite = emptyTextures[loadTextureIndex];
+        }
+    }
+}
